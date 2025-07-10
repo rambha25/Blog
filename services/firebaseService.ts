813e37d6310c38cd68c248085a4a244bbae57fc7
rambha25 <rambah8754@gmail.com>
2025-07-10 16:24:58 +0530

    firebaseService.ts

---
+++ services/firebaseService.ts
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBbkTVYt8t0w3wR_YlrgPhkK2uLLLDVgmU",
+  authDomain: "infobharatka-5d57f.firebaseapp.com",
+  projectId: "infobharatka-5d57f",
+  storageBucket: "infobharatka-5d57f.firebasestorage.app",
+  messagingSenderId: "601385946432",
+  appId: "1:601385946432:web:d9e0b634c9bf769eb73ea5",
+  measurementId: "G-JYT6V9BJDQ"
+};
