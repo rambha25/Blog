729a69aae69178db8c3b1cc353bfe1ab3aa2838d
rambha25 <rambah8754@gmail.com>
2025-07-11 08:53:49 +0530

    firebaseConfig.ts

---
+++ firebaseConfig.ts
@@ -0,0 +1,5 @@
+// This file has been moved to /services/firebaseConfig.ts
+// Please update your Firebase configuration in the new file.
+// This file can be safely deleted.
+
+export const firebaseConfig = {};
