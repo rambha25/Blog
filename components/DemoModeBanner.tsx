20fad422180b58d0882d52425874f9a4a477567a
rambha25 <rambah8754@gmail.com>
2025-07-11 08:54:29 +0530

    DemoModeBanner.tsx

--- components/DemoModeBanner.tsx
+++ components/DemoModeBanner.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const DemoModeBanner: React.FC = () => {
   return (
     <div className="bg-yellow-400 text-yellow-900 text-center p-2 font-semibold">
-      आप अभी डेमो मोड में हैं। आपका डेटा सेव नहीं होगा। Firebase को कॉन्फ़िगर करने के लिए <code className="bg-yellow-500 text-white px-1 rounded-sm">src/firebaseConfig.ts</code> फ़ाइल देखें।
+      आप अभी डेमो मोड में हैं। आपका डेटा सेव नहीं होगा। Firebase को कॉन्फ़िगर करने के लिए <code className="bg-yellow-500 text-white px-1 rounded-sm">services/firebaseConfig.ts</code> फ़ाइल देखें।
     </div>
   );
 };
